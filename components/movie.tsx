d7b1ce3904a00c0c2e8916878476c83113703d06
jh.gil <kjh979go@gmail.com>
2024-09-06 16:04:56 +0900

    tailwind 추가

--- components/movie.tsx
+++ components/movie.tsx
@@ -17,7 +17,7 @@ export default function Movie({title, id, poster_path}: IMovieProps) {
   }
   return (
     <div key={id} className={styles.movie}>
-      <img src={poster_path} alt={title} onClick={() => onClick(id)} />
+      <img src={poster_path} alt={title} onClick={() => onClick(id)} className="hover:border-2 border-red-800 border-solid" />
       <Link prefetch href={`/movies/${id}`}>{title}</Link>
     </div>
   )
